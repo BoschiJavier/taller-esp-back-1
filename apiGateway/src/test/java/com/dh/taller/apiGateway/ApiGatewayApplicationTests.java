22fc03ee38d5bbb89976137bbf8bc78d65727eb7
BoschiJavier <boschi.javier@gmail.com>
2022-11-07 14:55:03 -0300

    agregue config, gateway, eureka

---
+++ apiGateway/src/test/java/com/dh/taller/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.taller.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
